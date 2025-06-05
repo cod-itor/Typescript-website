68e574617717f9def479480bb504141fc68ce616
Dita Rector <ditarector@ToTo.local>
2025-06-05 11:56:19 +0700

    setting up the file structure

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let car = "BMW";
+let year = 2022;
+let isElectric = false;
+let carDetails = {
+    brand: car,
+    year: year,
+    isElectric: isElectric
+};
+console.log(`Car Details: ${carDetails.brand}, Year: ${carDetails.year}, Electric: ${carDetails.isElectric}`);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let car:String = "BMW";
+let year:number = 2022;
+let isElectric:boolean = false;
+let carDetails:any = {
+    brand: car,
+    year: year,
+    isElectric: isElectric
+};
+console.log(`Car Details: ${carDetails.brand}, Year: ${carDetails.year}, Electric: ${carDetails.isElectric}`);
